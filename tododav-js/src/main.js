0f73bcee755f35ba1228b182da1e0c3bc158b78c
Ben Trask <bentrask@comcast.net>
2015-12-04 15:38:31 -0500

    Simple functions for parsing and generating items.

--- tododav-js/src/main.js
+++ tododav-js/src/main.js
@@ -9,3 +9,15 @@ repo.query("", {}, function(err, URIs) {
 	console.log(err, URIs);
 });
 
+function formatItem(parentURI, content) {
+	return parentURI+"\r\n\r\n"+content;
+}
+function parseItem(item) {
+	var x = /^(|hash:\/\/[\w\d.-]+\/[\w\d.%_-]+)(?:\r\n|\r|\n)(?:\r\n|\r|\n)(.*)$/.exec(item);
+	if(!x) return null;
+	return {
+		parentURI: x[1],
+		content: x[2],
+	};
+}
+
