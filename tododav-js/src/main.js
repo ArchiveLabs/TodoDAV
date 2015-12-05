ecf3e9ac6e262f46b86171d19b1ed8f354c4ef36
Ben Trask <bentrask@comcast.net>
2015-12-05 14:24:41 -0500

    General cleanup and refactoring.

--- tododav-js/src/main.js
+++ tododav-js/src/main.js
@@ -6,6 +6,7 @@ var crypto = require("crypto");
 var sln = require("./sln-client");
 
 var TYPE = "text/vnd.tododav.item; charset=utf-8";
+var QUERY = "type='"+TYPE+"'"; // TODO: Dynamic queries require a bit more work.
 
 var repo = sln.createRepo("/", null);
 
@@ -29,6 +30,38 @@ function clone(id, childByID) {
 				findIDsInElement(children[i]);
 		})(element);
 	return element;
+}
+
+// CRDT functions
+function flagstate(crdt) {
+	var updates = crdt ? Object.keys(crdt) : [];
+	updates.sort(); // Lexicographic sort.
+	for(var i = updates.length; i-- > 0;) {
+		if(/\d{20}/.test(updates[i])) return updates[i];
+	}
+	return new Array(20+1).join("0");
+}
+function flaginc(flag) {
+	var digits = flag.split("").map(function(x) { return parseInt(x, 10); });
+	for(var i = digits.length; i-- > 0;) {
+		if(digits[i] < 9) {
+			digits[i]++;
+			return digits.map(function(x) { return x.toString(10); }).join("");
+		} else {
+			digits[i] = 0;
+		}
+	}
+	return flag; // Overflow.
+}
+function merge(src, dst) { // TODO: sln.mergeMeta ?
+	for(var x in src) if(has(src, x)) {
+		if(!has(dst, x)) dst[x] = {};
+		if("string" === typeof src[x]) {
+			dst[x][src[x]] = {};
+		} else {
+			merge(src[x], dst[x]);
+		}
+	}
 }
 
 function Item(parentURI, content) {
@@ -60,36 +93,6 @@ Item.prototype.getURI = function() {
 	sha256.end();
 	return "hash://sha256/"+sha256.read().toString("hex");
 };
-function flagstate(crdt) {
-	var updates = crdt ? Object.keys(crdt) : [];
-	updates.sort(); // Lexicographic sort.
-	for(var i = updates.length; i-- > 0;) {
-		if(/\d{20}/.test(updates[i])) return updates[i];
-	}
-	return new Array(20+1).join("0");
-}
-function flaginc(flag) {
-	var digits = flag.split("").map(function(x) { return parseInt(x, 10); });
-	for(var i = digits.length; i-- > 0;) {
-		if(digits[i] < 9) {
-			digits[i]++;
-			return digits.map(function(x) { return x.toString(10); }).join("");
-		} else {
-			digits[i] = 0;
-		}
-	}
-	return flag; // Overflow.
-}
-function merge(src, dst) { // TODO: sln.mergeMeta ?
-	for(var x in src) if(has(src, x)) {
-		if(!has(dst, x)) dst[x] = {};
-		if("string" === typeof src[x]) {
-			dst[x][src[x]] = {};
-		} else {
-			merge(src[x], dst[x]);
-		}
-	}
-}
 Item.prototype.getState = function() {
 	var item = this;
 	var state = flagstate(item.meta["completed"]);
@@ -142,7 +145,7 @@ Item.load = function(URI, cb) {
 			if(err) return cb(err, null);
 			merge(info, item.meta);
 			// TODO: We should record the meta-files here, but the
-			// API doesn't give them. Doesn't matter much though.
+			// API doesn't give them. Minor performance hit.
 			item.internal.checkbox.checked = item.getState();
 			return cb(null, item);
 		});
@@ -182,50 +185,27 @@ document.getElementById("new").onchange = function(e) {
 var main = document.getElementById("main");
 var latestMeta = null;
 var latestFile = null;
+var loadedWindow = false;
+var loadedInitial = false;
 
-// TODO: We need to be careful about race conditions when first loading.
-// 1. Start watching for meta-files
-// 2. Record all meta-files seen
+// We need to be careful about race conditions when first loading.
+// 1. Choose a recent meta-file to start from
 // 3. Load latest files
 // 4. Apply (possibly redundant) meta-file updates
-// Also the case of reconnection requires us to be similarly careful.
+// TODO: Also the case of reconnection requires us to be similarly careful.
 var updates = repo.createMetafilesStream({ count: 1, start: "", wait: false, dir: "z" });
 updates.on("data", function(info) {
 	latestMeta = info.uri;
 });
 updates.on("end", function() {
 
-
-// TODO: This query is a hack.
-// We should also allow custom queries?
-var stream = repo.createQueryStream("type='"+TYPE+"'", { count: 50, wait: false, dir: "z" })
-stream.on("data", function(URI) {
-
-	if(!latestFile) latestFile = URI;
-
-	var tmp = clone("loading", null);
-	main.appendChild(tmp);
-
-	Item.load(URI, function(err, item) {
-		if(err) {
-			main.removeChild(tmp);
-			console.log(err);
-			stream.resume();
-			return;
-		}
-		main.replaceChild(item.element, tmp);
-	});
-
-});
-stream.on("end", function() {
-
-	stream = repo.createQueryStream("type='"+TYPE+"'", { start: latestFile, wait: true });
+	var stream = repo.createQueryStream(QUERY, { count: 50, wait: false, dir: "z" })
 	stream.on("data", function(URI) {
 
-		latestFile = URI;
+		if(!latestFile) latestFile = URI;
 
 		var tmp = clone("loading", null);
-		main.insertBefore(tmp, main.childNodes[0]);
+		main.appendChild(tmp);
 
 		Item.load(URI, function(err, item) {
 			if(err) {
@@ -237,20 +217,41 @@ stream.on("end", function() {
 			main.replaceChild(item.element, tmp);
 		});
 
-
+	});
+	stream.on("end", function() {
+		loadedInitial = true;
+		if(loadedWindow) receiveUpdates()
 	});
 
+});
+window.onload = function() {
+	loadedWindow = true;
+	if(loadedInitial) receiveUpdates();
+};
 
-	var updates = repo.createMetafilesStream({ start: latestMeta, wait: true });
-	updates.on("data", function(info) {
+function receiveUpdates() {
+	var files = repo.createQueryStream(QUERY, { start: latestFile, wait: true });
+	files.on("data", function(URI) {
+		latestFile = URI;
+
+		var tmp = clone("loading", null);
+		main.insertBefore(tmp, main.childNodes[0]);
+
+		Item.load(URI, function(err, item) {
+			if(err) {
+				main.removeChild(tmp);
+				console.log(err);
+				return;
+			}
+			main.replaceChild(item.element, tmp);
+		});
+	});
+	var metas = repo.createMetafilesStream({ start: latestMeta, wait: true });
+	metas.on("data", function(info) {
+		latestMeta = info.uri;
+
 		var item = Item.byURI[info.target];
 		if(item) item.update(info.uri, function(){});
-		latestMeta = info.uri;
 	});
-
-
-});
-
-
-});
+}
 
